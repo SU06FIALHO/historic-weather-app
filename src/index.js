652fa49a6664d27aea3b0d1e92c5b3ecca996531
SU06FIALHO <sfialho50@gmail.com>
2024-03-30 21:39:41 +0000

    Added search functionality using the API

--- src/index.js
+++ src/index.js
@@ -1,8 +1,25 @@
+function updateWeather(response) {
+  let tempElement = document.querySelector("#temper");
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#city-display");
+  cityElement.innerHTML = response.data.city;
+  tempElement.innerHTML = Math.round(temperature);
+}
+
+function apiSearchCity (city) {
+let apiKey = "74ba2ac4o95a095ca8b15001f53d3et4";
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+axios.get(apiUrl).then(updateWeather);
+}
+
+
 function submitCitySearch (event) {
   event.preventDefault();
   let citySearchForm = document.querySelector("#city-search-form");
-  let cityElement = document.querySelector ("#city-display");
-  cityElement.innerHTML= citySearchForm.value;
+  
+  apiSearchCity(citySearchForm.value);
 }
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener ("submit", submitCitySearch);
+
+apiSearchCity ("Lisbon");
