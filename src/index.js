bccc3b9012787a959d7ad5319d5f560ad739a50b
SU06FIALHO <sfialho50@gmail.com>
2024-03-31 21:56:26 +0100

    Added more weather data (day, hours, minutes, humidity and wind-speed)

--- src/index.js
+++ src/index.js
@@ -2,8 +2,36 @@ function updateWeather(response) {
   let tempElement = document.querySelector("#temper");
   let temperature = response.data.temperature.current;
   let cityElement = document.querySelector("#city-display");
+  let detailsElement = document.querySelector("#weather-details");
+  let humidityElement = document.querySelector("#humidity-value");
+  let windElement = document.querySelector("#wind-value");
+  let timeElement = document.querySelector("#time-value");
+  let date = new Date(response.data.time * 1000);
+
+  timeElement.innerHTML = displayDate(date);
+  humidityElement.innerHTML = `${response.data.temperature.humidity} %`;
+  windElement.innerHTML = `${response.data.wind.speed} km/h`;
+  detailsElement.innerHTML = response.data.condition.description;
+  
   cityElement.innerHTML = response.data.city;
   tempElement.innerHTML = Math.round(temperature);
+  
+  
+}
+
+function displayDate(date) {
+  
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  
+  let hours = date.getHours();
+  let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+
+  let day = days [date.getDay()];
+  return `${day} ${hours}:${minutes},`;
 }
 
 function apiSearchCity (city) {
@@ -19,6 +47,7 @@ function submitCitySearch (event) {
   
   apiSearchCity(citySearchForm.value);
 }
+
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener ("submit", submitCitySearch);
 
